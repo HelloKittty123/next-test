de63a9e58bc0ea9ccd3a2664f2ae092c56806c22
HelloKittty123 <92498918+HelloKittty123@users.noreply.github.com>
2026-01-19 09:23:17 +0700

    UPDATE_nextt_config

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,23 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["demo-2025-eaccount-user-image-second.s3-han02.fptcloud.com"],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@messages": path.resolve(__dirname, "messages"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
+      "@models": path.resolve(__dirname, "src/models"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@layouts": path.resolve(__dirname, "src/layouts"),
+      "@types": path.resolve(__dirname, "src/types"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+    };
+    return config;
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
